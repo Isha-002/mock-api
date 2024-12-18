464401ae4f845df789e862e338de42cf88a26eba
Isha-002 <narges1353zeinali@gmail.com>
2024-12-19 00:09:11 +0330

    adding new function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 struct Restaurant {
     id: RestaurantId,
     name: String,
-    rating: u8,
+    rating: f32,
     distance: f64,
     tags: Option<Vec<String>>,
     image: String,
@@ -13,7 +13,7 @@ impl Restaurant {
     fn new(
         id: RestaurantId,
         name: String,
-        rating: u8,
+        rating: f32,
         distance: f64,
         tags: Option<Vec<String>>,
         image: String,
@@ -27,8 +27,21 @@ impl Restaurant {
             image,
         }
     }
+    fn update_name(&self, new_name: String) -> Self {
+        Restaurant::new(self.id, new_name, self.rating, self.distance, self.tags, self.image)
+    }
 }
 
+
+
 fn main() {
+    let res = Restaurant::new(
+        RestaurantId("1".to_string()),
+        "kebab".to_string(),
+        3.5,
+        2.5,
+        Some(vec!["aaa".to_string(), "aaa".to_string()]),
+        "img".to_string(),
+    );
     println!("Hello, world!");
 }
