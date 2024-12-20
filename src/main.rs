3edae9aa78780708d3e69d880f001e0b7f2dd6e4
Isha-002 <narges1353zeinali@gmail.com>
2024-12-21 00:56:15 +0330

    implementing Display Traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{fmt::{self}, io::{Error, ErrorKind}, str::FromStr};
+
 struct Restaurant {
     id: RestaurantId,
     name: String,
@@ -7,6 +9,7 @@ struct Restaurant {
     image: String,
 }
 
+#[derive(Debug, Clone)]
 struct RestaurantId(String);
 
 impl Restaurant {
@@ -15,7 +18,7 @@ impl Restaurant {
         name: &str,
         rating: f32,
         distance: f64,
-        tags: Option<Vec<&str>>,
+        tags: Option<Vec<String>>,
         image: &str,
     ) -> Self {
         Restaurant {
@@ -23,25 +26,59 @@ impl Restaurant {
             name: name.to_string(),
             rating,
             distance,
-            tags: tags.map(|t| t.into_iter().map(|s| s.to_string()).collect()),
+            tags,
             image: image.to_string(),
         }
     }
     fn update_name(&self, new_name: &str) -> Self {
-        Restaurant::new(self.id, new_name, self.rating, self.distance, self.tags, &self.image)
+        Restaurant::new(
+            self.id.clone(),
+            new_name,
+            self.rating,
+            self.distance,
+            self.tags.clone(),
+            &self.image,
+        )
+    }
+}
+
+impl fmt::Display for Restaurant {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "`{{ id: {}, name: {}, rating: {}, distance: {}, tags: {:?}, image: {} }}`",
+            self.id, self.name, self.rating, self.distance, self.tags, self.image
+        )
     }
 }
 
 
+impl fmt::Display for RestaurantId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromStr for RestaurantId {
+    type Err = std::io::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(RestaurantId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
 
 fn main() {
     let res = Restaurant::new(
-        RestaurantId("1".to_string()),
-        "kebab".to_string(),
+        RestaurantId::from_str("1").expect("No id provided"),
+        "kebab",
         3.5,
         2.5,
         Some(vec!["aaa".to_string(), "aaa".to_string()]),
-        "img".to_string(),
+        "img",
     );
-    println!("Hello, world!");
+    println!("Hello, world!, {}", res);
 }
+
+// 52
